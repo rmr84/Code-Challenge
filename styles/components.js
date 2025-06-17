19217abc740be7fd4207d410349617f0a2f6f3ca
rruffing@wynhouse.co <rruffing@wynhouse.co>
2025-06-17 19:53:58 -0400

    add a components file for the button

---
+++ styles/components.js
@@ -0,0 +1,12 @@
+export const components = {
+    button: {
+      borderRadius: 8,
+      justifyContent: "center",
+      borderColor: "#282646",
+      fontFamily: "eurof55",
+      borderWidth: 1,
+      width: 320,
+      marginTop: 20
+    }
+  };
+  
