e9a272e29f54b30248df97ab503fee3311b650e0
rruffing@wynhouse.co <rruffing@wynhouse.co>
2025-06-17 19:56:30 -0400

    don't need current screen check here.

--- components/Navigation.js
+++ components/Navigation.js
@@ -6,24 +6,13 @@ import { SignIn } from "../screens/SignIn";
 import { OTP } from "../screens/OTP";
 import { Dashboard } from "../screens/Dashboard";
 import { ForgotPassword } from "../screens/ForgotPassword";
+
 import Authentication from "./Authentication";
 const { Navigator, Screen } = createStackNavigator();
 export default function Navigation() {
-  const [currentScreen, setCurrentScreen] = useState("Dashboard");
-
   return (
-    <NavigationContainer
-      onStateChange={(state) =>
-        setCurrentScreen(
-          state.routes[state.index].state
-            ? state.routes[state.index].state.routes[
-                state.routes[state.index].state.index
-              ].name
-            : state.routes[state.index].name
-        )
-      }
-    >
-      <Authentication currentScreen={currentScreen} />
+    <NavigationContainer>
+      <Authentication />
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name={"SignIn"} component={SignIn} />
         <Screen name={"SignUp"} component={SignUp} />
