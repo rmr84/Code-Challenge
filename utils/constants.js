75548259f56d66316422ae5ca7ed8a653cb8e5ff
rruffing@wynhouse.co <rruffing@wynhouse.co>
2025-06-17 19:54:06 -0400

    adding a constants file

---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+export const openers = [
+    "Every day's story starts here. Ready to write yours?",
+    "Catch your thoughts before they fly away.",
+    "Ready to spill some thoughts? Your journal’s all ears",
+    "Pause. Reflect. Write.",
+]
