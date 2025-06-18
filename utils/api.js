ee551dc6ae17587ab3d1a5fa0e98864a6686b7f0
rruffing@wynhouse.co <rruffing@wynhouse.co>
2025-06-17 23:05:43 -0400

    entries endpoints

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// USERS
+
+// Post operation will create an entry in Mongo that has createdAt, updatedAt, _id, and firebaseId
 export const createUserAPI = async (data) => {
   return await axios
     .post(`http://localhost:3000/api/users`, data)
@@ -9,9 +12,32 @@ export const createUserAPI = async (data) => {
     });
 };
 
+// Can query by the firebase id (uid)
 export const getUsersAPI = async (params) => {
   return await axios
     .get(`http://localhost:3000/api/users`, { params })
+    .then((response) => response)
+    .catch((error) => {
+      console.error(JSON.stringify(error));
+    });
+};
+
+// JOURNAL ENTRIES
+
+// Post operation will create an entry in Mongo that has createdAt, updatedAt, _id, and userId
+export const createEntryAPI = async (data) => {
+  return await axios
+    .post(`http://localhost:3000/api/entries`, data)
+    .then((response) => response)
+    .catch((error) => {
+      console.error(JSON.stringify(error));
+    });
+};
+
+// Can query by the user's id that is on the entry object to match journal entries to a user
+export const getEntriesAPI = async (params) => {
+  return await axios
+    .get(`http://localhost:3000/api/entries`, { params })
     .then((response) => response)
     .catch((error) => {
       console.error(JSON.stringify(error));
